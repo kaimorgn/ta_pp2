5f2583a907f2b0e633965ad93bb780597b153579
kai@junin <kai.yoshida@cps.akita-pu.ac.jp>
2025-09-22 20:51:31 +0900

    pathlibディレクトリ

---
+++ pathlib/template.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#
+# XXX.py
+# 
+# [概要]
+#
+#
+#
+#
+#
+
+import os
+
+
+class Template:
+    def __init__(self):
+        pass
+
+    def template(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
