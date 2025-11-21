d9d4201d2a64f2c18816a3487ddc7c1b47596161
kai@junin <kai.yoshida@cps.akita-pu.ac.jp>
2025-11-21 13:53:54 +0900

    pillowディレクトリを作成

---
+++ pillow/make_invitation_card.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#
+# make_invitation_card.py
+#
+# [概要]
+#
+#
+#
+#
+
+from PIL import Image
+
+from features.setup_logging import setup_logging
+
+from logging import getLogger
+from pathlib import Path
+
+# logging の設定を反映してロガーを作成
+setup_logging()
+logger = getLogger(__name__)
+
+
+if __name__ == "__main__":
+    pass
