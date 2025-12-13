c811d335030ed3e930ca432ab0e3bc22207cfe50
kai@junin <kai.yoshida@cps.akita-pu.ac.jp>
2025-12-13 17:20:12 +0900

    スプレッドシートを辞書型で取得: OK

--- ezsheets/features/sync_with_google_sheets.py
+++ ezsheets/features/sync_with_google_sheets.py
@@ -29,20 +29,52 @@ class SyncWithGoogleSheets:
     def _refresh_token(self, secret_file):
         '''
         [概要]
-        client_secret.jsonを使ってトークンを発行するためのメソッド．
+        client_secret_*.jsonを使ってトークンを発行するためのメソッド．
         インスタンス生成と同時に実行される．
         '''
-        assert secret_file, "無効なclient_secret.jsonのパスが渡されました"
-        assert isinstance(secret_file, Path), "Pathオブジェクトを渡して"
+        assert secret_file, "client_secret.jsonのパスを確認してください"
+        assert isinstance(secret_file, Path), "Pathオブジェクトにしてください"
 
         try:
-            logger.debug(">>> 新しいトークンを発行します")
+            logger.debug("> 新しいトークンを発行します")
             ezsheets.init()
-            logger.debug(">>> 新しいトークンの発行に成功しました")
+            logger.debug("> 新しいトークンの発行に成功しました")
+            logger.info(">> 認証に成功しました")
 
         except Exception as e:
             logger.error(
                 f"新しいトークンの発行時にエラーが発生しました: {e}"
+            )
+            raise e
+
+    def _make_sheets_dict(self):
+        '''
+        '''
+        try:
+            logger.debug("> 複数のスプレッドシートを取得します")
+            self.sheets_dict = ezsheets.listSpreadsheets()
+            if not self.sheets_dict:
+                logger.warning(
+                    f"シートを取得できなかった: {len(self.sheets_dict)}"
+                )
+                raise
+
+            logger.debug("> 複数のスプレッドシートを取得しました")
+            logger.info(
+                f">> {len(self.sheets_dict)}個のスプレッドシートを取得しました"
+            )
+            
+            logger.debug("> コンソール上に出力します")
+            for sheet_id, title in self.sheets_dict.items():
+                logger.debug(
+                    f"シート名: {title}, \nシートID: {sheet_id}"
+                )
+
+            return True
+
+        except Exception as e:
+            logger.error(
+                f"複数のスプレッドシート取得時にエラーが発生しました: {e}"
             )
             raise e
 
@@ -56,4 +88,4 @@ if __name__ == "__main__":
     setup_logging(logging_config)
 
     secret_file = Path("./credentials-sheets.json")
-    SyncWithGoogleSheets(secret_file)
+    SyncWithGoogleSheets(secret_file)._make_sheets_dict()
